d41d950a1c54f8c7e170aa1fec7c5a89e7013194
Kenneth <obengmirekukenneth@gmail.com>
2025-08-14 19:55:08 +0000

    small fixes

---
+++ studentReportCard.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+class Student{
+    public:
+    string name;
+    string studentID;
+};
+
+
+int main()
+{
+
+    return 0;
+}
